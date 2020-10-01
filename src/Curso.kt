8afcba866160eec4c5ff3269dc9fce90455e50d4
NayaraGarcia <70548587+NayaraGarcia@users.noreply.github.com>
2020-10-01 15:21:16 -0300

    Parte E

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,2 +1,15 @@
-class Curso (var nome: String, var codigoCurso: Int) {
+import javax.naming.InitialContext
+
+class Curso( var nome: String, var codigoCurso: Int, var quantidadeMaxAluno :Int) {
+
+
+       override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (other !is Curso) return false
+
+        if (codigoCurso == other.codigoCurso) return true
+
+        return true
+    }
 }
