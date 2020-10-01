771393d013e859ffa26f7262acac6ce01abbe2f2
NayaraGarcia <70548587+NayaraGarcia@users.noreply.github.com>
2020-10-01 16:14:27 -0300

    Parte H: Método excluirUmAluno.

--- src/desafioDigitalHouse/Curso.kt
+++ src/desafioDigitalHouse/Curso.kt
@@ -5,6 +5,10 @@ class Curso(var nome: String,var codigoCurso: Int) {
     var alunos = mutableMapOf<Int, Aluno>()
     var vagasDisponiveis: Int = 0
 
+    constructor(nome: String, codigoCurso: Int, _vagasDisponiveis: Int) : this(nome, codigoCurso) {
+        vagasDisponiveis = _vagasDisponiveis
+    }
+
     fun adicionarUmAluno(umAluno: Aluno): Boolean{
         when {
             vagasDisponiveis > 0 -> {
@@ -15,6 +19,13 @@ class Curso(var nome: String,var codigoCurso: Int) {
         return false
     }
 
+    // Esta dando erro 
+    fun excluirAluno(umAluno: Aluno){
+        alunos.remove(umAluno)
+        println("Aluno ${umAluno.nome} ${umAluno.sobrenome} foi excluído. ")
+        vagasDisponiveis++
+    }
+    
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
 
