ba1b5537e0a322659a82025ae02ef3d0f15a67dd
NayaraGarcia <70548587+NayaraGarcia@users.noreply.github.com>
2020-10-02 19:14:16 -0300

    Cadastro

--- src/desafioDigitalHouse/PrincipalMain.kt
+++ src/desafioDigitalHouse/PrincipalMain.kt
@@ -9,10 +9,10 @@ fun main(){
 
     val manager = DigitalHouseManager(alunos, professores, cursos, matriculas)
 
-    manager.registrarProfessorTitular("Professor", "1", 1, "Especialidade 1")
-    manager.registrarProfessorTitular("Professor", "2", 2, "Especialidade 2")
-    manager.registrarProfessorAdjunto("Professor", "3", 3, 60)
-    manager.registrarProfessorAdjunto("Professor", "4", 4, 90)
+    manager.registrarProfessorTitular("Ana", "Garcia", 1, "Artes")
+    manager.registrarProfessorTitular("Marco", "Garcia", 2, "Geografia")
+    manager.registrarProfessorAdjunto("Rosa", "Garcia", 3, 100)
+    manager.registrarProfessorAdjunto("Thayane", "Garcia", 4, 150)
 
     manager.registrarCurso("Full Stack", 20001, 3)
     manager.registrarCurso("Android", 20002, 2)
@@ -20,14 +20,14 @@ fun main(){
     manager.alocarProfessores(20001, 1, 3)
     manager.alocarProfessores(20002, 2, 4)
 
-    manager.registrarAluno("Aluno", "1", 1)
-    manager.registrarAluno("Aluno", "2", 2)
+    manager.registrarAluno("Gabriela", "Oliveira", 1)
+    manager.registrarAluno("José", "Soares", 2)
     manager.matricularAluno(1, 20001)
     manager.matricularAluno(2, 20001)
 
-    manager.registrarAluno("Aluno", "3", 3)
-    manager.registrarAluno("Aluno", "4", 4)
-    manager.registrarAluno("Aluno", "5",  5)
+    manager.registrarAluno("Laura", "Silva", 3)
+    manager.registrarAluno("Maitê", "Yoo", 4)
+    manager.registrarAluno("Marlene", "Silva",  5)
     manager.matricularAluno(3, 20002)
     manager.matricularAluno(4, 20002)
     manager.matricularAluno(5, 20002)
